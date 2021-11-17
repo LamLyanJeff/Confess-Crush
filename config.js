23cd978057223889dd6bd0a381e46807b0445d93
LamLyanJeff <94499553+LamLyanJeff@users.noreply.github.com>
2021-11-17 09:48:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Hẽ lou',
+    introDesc: `Cùng chơi với mình 1 trò chơi nhá!!! UwU`,
+    btnIntro: 'OKEYYYY!!!',
+    title: 'Cứ trả lời 1 cách tự nhiên nháaa!!! ><',
+    desc: 'Pạn có thấy mình mập khôm :33??? ',
+    btnYes: 'Khôm nhaaa <33',
+    btnNo: 'Có gất nhiều :DD',
+    question: 'Sao lại thấy dzậy dọ?? UwU',
+    btnReply: 'Gửi cho pạn nà <3',
+    reply: 'Vì pạn cute thấy mồ <333333333',
+    mess: 'Mình pít mà. Yêu bạn nhiều nhiềuuuuu!!! ><',
+    messDesc: 'Sao còn khôm quay lại nhắn cho mình nàaa :33',
+    btnAccept: 'Okeyy nuôn <3',
+    messLink: 'https://www.facebook.com/lananh.nguyenngoc.1276487' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
